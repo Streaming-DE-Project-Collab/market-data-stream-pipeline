b83f76bf194bdfbd8c6c897a51fe1ba7ef730d95
ckmonster9 <ckmonster332@gmail.com>
2023-12-18 23:09:20 -0800

    iex kafka ingestion

---
+++ iex-stream-requests.py
@@ -0,0 +1,32 @@
+from iexfinance.stocks import Stock, get_historical_data
+from kafka import KafkaProducer
+import json
+import os
+import time
+iex_token = os.getenv('IEX_TOKEN')
+
+output_format = 'json' # or pandas
+symbols = ["TSLA", "AAPL", "GOOG"]
+call_time_limit = 0.5 # in seconds
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+topic_name = 'stock_prices'
+
+#aapl = Stock('AAPL', output_format='json', token = iex_token)
+
+while True:
+    start_time = time.time()
+    for s in symbols: 
+        result = Stock(s, output_format = output_format)
+        price = result.get_price()
+        print(s, price)
+        #print(result.get_key_stats())
+        producer.send(topic_name, {'symbol': s, 'price': price})
+    while time.time() - start_time < call_time_limit:
+        time.sleep(1)
+    end_time = time.time()
+    print(f"Time taken to execute: {round(end_time - start_time,2)} seconds")
