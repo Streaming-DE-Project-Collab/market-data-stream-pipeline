4e58c8c592738def066ab22a7b47ffe803ba680d
ckmonster9 <ckmonster332@gmail.com>
2023-12-19 22:30:33 -0800

    add timestamp

--- iex-stream-requests.py
+++ iex-stream-requests.py
@@ -3,6 +3,7 @@ from kafka import KafkaProducer
 import json
 import os
 import time
+from datetime import datetime
 iex_token = os.getenv('IEX_TOKEN')
 
 output_format = 'json' # or pandas
@@ -20,12 +21,13 @@ topic_name = 'stock_prices'
 
 while True:
     start_time = time.time()
+    #print("start_time", datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S'))
     for s in symbols: 
         result = Stock(s, output_format = output_format)
         price = result.get_price()
         print(s, price)
         #print(result.get_key_stats())
-        producer.send(topic_name, {'symbol': s, 'price': price})
+        producer.send(topic_name, {'symbol': s, 'price': price, 'timestamp': datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')})
     while time.time() - start_time < call_time_limit:
         time.sleep(1)
     end_time = time.time()
